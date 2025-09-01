657833d13067dbe7200b06113045197b587bdfea
Florent Chaillie <florentchaillie@gmail.com>
2025-09-01 17:56:05 +0200

    chaging first graph title by adding perf

--- app/main.py
+++ app/main.py
@@ -307,7 +307,7 @@ else:
         f"""
         <div style="background: white; padding: 4px 12px; margin: 4px 0 18px 0; text-align: center;
                     font-size: 2.0rem; font-weight: bold; color: black; box-shadow: 0 2px 6px rgba(0,0,0,0.06);">
-            Starting portfolio equally weighted backtest performance:{eq_perf_value}
+            Starting portfolio equally weighted backtest performance: {eq_perf_value}%
         </div>
         """,
         unsafe_allow_html=True,
